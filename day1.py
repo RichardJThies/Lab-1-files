ee399afa4899e3553499833367a0f057976256ba
Thies <pd3514sr@go.minneapolis.edu>
2023-08-23 19:45:35 -0500

    Initial commit

---
+++ day1.py
@@ -0,0 +1,30 @@
+## Part 1
+print('What is your name?')
+userName = input()
+print('What month were you born in?')
+monthAnswer = input()
+print('Hello ' + userName + '!')
+nameLength = len(userName)
+print('Your name has ' + nameLength + ' letters.')
+
+
+
+
+# birthMonth = 'May'
+# if birthMonth == 'May':
+#     print('Happy birth month!')
+# else:
+#     print('TEST')
+
+
+
+
+
+
+
+
+
+
+
+
+
